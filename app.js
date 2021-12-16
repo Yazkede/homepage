958807489d6bb30c5f76ff42b5423f711139a4ac
Alexandr Rybin <gelikopterfl@gmail.com>
2021-12-17 01:12:25 +0200

    resume creating

---
+++ app.js
@@ -0,0 +1,17 @@
+const CONTAINER = document.querySelector('.container');
+const LIGHTBTN = document.querySelector('#light');
+const DARKBTN = document.querySelector('#dark');
+
+LIGHTBTN.addEventListener('click', changeMode);
+DARKBTN.addEventListener('click', changeMode);
+
+function changeMode(e) {
+    if (e.target == DARKBTN) {
+        CONTAINER.classList.remove('light');
+        CONTAINER.classList.add('dark');
+    }
+    if (e.target == LIGHTBTN) {
+        CONTAINER.classList.remove('dark');
+        CONTAINER.classList.add('light');
+    }
+}
